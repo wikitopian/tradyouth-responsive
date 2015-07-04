594c88742f1a32bf9191dd72ec6e9bbaef550c5d
wikitopian <matt@swarmstrategies.com>
2015-07-04 01:40:46 +0000

    import roller theme

--- functions.php
+++ functions.php
@@ -11,13 +11,26 @@ class TradYouth_Responsive {
 	public function do_styles() {
 
 		$parent = 'mildendo';
+		$child  = 'tradyouth_responsive';
 
 		wp_enqueue_style( $parent, get_template_directory_uri() . '/style.css' );
 
 		wp_enqueue_style(
-			'tradyouth_responsive',
+			'roller',
+			get_stylesheet_directory_uri() . '/roller.css',
+			array( 'jquerymobile', $parent )
+		);
+
+		wp_enqueue_style(
+			'roller_icons',
+			get_stylesheet_directory_uri() . '/jquery.mobile.icons.min.css',
+			array( 'jquerymobile', $parent, 'roller' )
+		);
+
+		wp_enqueue_style(
+			$child,
 			get_stylesheet_directory_uri() . '/style.css',
-			array( $parent )
+			array( 'jquerymobile', $parent, 'roller', 'roller_icons' )
 		);
 
 	}
